945ffbb190ae628d8f0754c6eb3f5bd8f39f24f4
Sahil Abrar <zeroxkhan2@gmail.com>
2023-10-08 11:18:59 +0600

    Added personalities section in home page

--- zerox-arcade/src/Components/Home/Home.jsx
+++ zerox-arcade/src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
 import Hero from './../Hero/Hero';
+import Personalities from '../Personalities/Personalities';
 
 const Home = () => {
   const card = useLoaderData();
@@ -12,8 +13,9 @@ const Home = () => {
     <>
       <Hero></Hero>
       <Banner></Banner>
-      <h1 className="mx-auto text-center text-4xl font-bold text-white mt-5">
-        Our Services
+      <Personalities></Personalities>
+      <h1 className="mx-auto text-center text-4xl font-bold text-white mt-20">
+        Our <span className="text-rose-700">Services</span>
       </h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10 grid-cols-1">
         {card?.map(allCard => (
---
+++ zerox-arcade/src/Components/Personalities/Personalities.jsx
@@ -0,0 +1,70 @@
+const Personalities = () => {
+  return (
+    <div className="text-center font-bold text-white text-3xl mt-10">
+      <p>Personalities</p>
+      <div className="items-center justify-center flex md:flex-row lg:flex-row flex-col gap-10 mt-10 text-center">
+        <img
+          className="w-[200px] h-[200px] rounded-full"
+          src="https://i.ibb.co/YQYDqVL/merlin-158588154-1afb43db-5ae0-45ab-ad7c-7a61edb57f3a-video-Sixteen-By-Nine3000.jpg"
+          alt=""
+        />
+        <div className="text-left">
+          <p className="text-white text-3xl font-bold">Shroud</p>
+          <p className="text-xs mt-5 text-white max-w-[400px]">
+            Michael Grzesiek, widely known as Shroud, is a prominent figure in
+            the gaming world, celebrated for his extraordinary skills and
+            charismatic streaming presence. With a background in professional
+            esports, Shroud has become an influential content creator,
+            captivating audiences with his remarkable gameplay across various
+            titles. His dedication to excellence and engaging personality have
+            earned him a massive following and a lasting legacy in the gaming
+            community.
+          </p>
+        </div>
+      </div>
+
+      <div className="items-center justify-center flex md:flex-row lg:flex-row flex-col gap-10 mt-10 text-center">
+        <img
+          className="w-[200px] h-[200px] rounded-full"
+          src="https://i.ibb.co/rcCysGY/8a171d167d.jpg"
+          alt=""
+        />
+        <div className="text-left">
+          <p className="text-white text-3xl font-bold">Darren Elias</p>
+          <p className="text-xs mt-5 text-white max-w-[400px]">
+            Darren Elias is a renowned professional poker player, hailing from
+            the United States, known for his exceptional skills and numerous
+            tournament victories. With a remarkable ability to read opponents
+            and make strategic decisions, he has solidified his place as one of
+            the poker worlds most respected figures, consistently earning
+            impressive cash prizes throughout his career. Elias dedication to
+            the game and his analytical approach make him a true inspiration for
+            aspiring poker enthusiasts.
+          </p>
+        </div>
+      </div>
+
+      <div className="items-center justify-center flex md:flex-row lg:flex-row flex-col gap-10 mt-10 text-center">
+        <img
+          className="w-[200px] h-[200px] rounded-full"
+          src="https://i.ibb.co/Lp3bhpY/istockphoto-1394310658-612x612.jpg"
+          alt=""
+        />
+        <div className="text-left">
+          <p className="text-white text-3xl font-bold">Ninja</p>
+          <p className="text-xs mt-5 text-white max-w-[400px]">
+            Ninja, whose real name is Tyler Blevins, is a renowned professional
+            gamer and streamer known for his exceptional skills in games like
+            Fortnite and Valorant. With millions of followers on platforms like
+            Twitch and YouTube, he has become a gaming icon, inspiring countless
+            aspiring gamers worldwide. Beyond his gaming prowess, Ninja is also
+            recognized for his charitable efforts and contributions to the
+            gaming community.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Personalities;
