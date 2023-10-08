1c4d5ae5e5676cf2bbda4094793a07c7c894d81d
Sahil Abrar <zeroxkhan2@gmail.com>
2023-10-08 09:59:02 +0600

    Added dashboard details

---
+++ zerox-arcade/src/Components/Dashboard/ChartS.jsx
@@ -0,0 +1,29 @@
+import { Chart } from 'react-google-charts';
+
+const ChartS = () => {
+  const data = [
+    ['day', 'a', 'b', 'c', 'd'],
+    ['Mon', 20, 28, 38, 45],
+    ['Tue', 31, 38, 55, 66],
+    ['Wed', 50, 55, 77, 80],
+    ['Thu', 50, 77, 66, 77],
+    ['Fri', 15, 66, 22, 68],
+  ];
+
+  const options = {
+    legend: 'none',
+  };
+
+  return (
+    <Chart
+      chartType="CandlestickChart"
+      width="100%"
+      className="bg-transparent"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+};
+
+export default ChartS;
--- zerox-arcade/src/Components/Dashboard/Dashboard.jsx
+++ zerox-arcade/src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,66 @@
 const Dashboard = () => {
   return (
     <div>
-      <h1>This is dashboard</h1>
+      <img
+        className="w-[300px] h-[300px] rounded-full mx-auto justify-center mt-9"
+        src="https://i.ibb.co/85zxjZR/Xin-Final-Paid-Revised.webp"
+        alt=""
+      />
+      <h1 className="mx-auto text-center font-bold text-3xl mt-10 text-white">
+        Event Join Pogression
+      </h1>
+
+      <div className="mt-10">
+        <input
+          type="range"
+          min={0}
+          max="100"
+          value="25"
+          className="range mx-auto text-center range-primary"
+          step="25"
+        />
+        <div className="w-full flex justify-between text-xs px-2">
+          <span className="text-white">|</span>
+          <span className="text-white">|</span>
+          <span className="text-white">|</span>
+          <span className="text-white">|</span>
+          <span className="text-white">|</span>
+        </div>
+      </div>
+
+      <h1 className="mx-auto text-center font-bold text-3xl mt-10 text-white">
+        User Condition
+      </h1>
+      <div className="mx-auto text-center font-bold text-3xl mt-10">
+        <div className="rating gap-1">
+          <input
+            type="radio"
+            name="rating-3"
+            className="mask mask-heart bg-red-400"
+          />
+          <input
+            type="radio"
+            name="rating-3"
+            className="mask mask-heart bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-3"
+            className="mask mask-heart bg-yellow-400"
+          />
+          <input
+            type="radio"
+            name="rating-3"
+            className="mask mask-heart bg-lime-400"
+            checked
+          />
+          <input
+            type="radio"
+            name="rating-3"
+            className="mask mask-heart bg-green-400"
+          />
+        </div>
+      </div>
     </div>
   );
 };
